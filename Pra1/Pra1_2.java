0c8f7464666cfde40f5202dc4fd73b1863f3066f
Yusuke Goto <yusukegoto7722@gmail.com>
2018-09-14 00:09:02 +0900

    Pro1 is finish.

---
+++ Pra1/Pra1_2.java
@@ -0,0 +1,9 @@
+public class Pra1_2{
+    public static void main(String[] args){
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("height is " + a + " " + "width is " + b + "." + " " + "Rectangular area is " + c + ".");
+        
+    }
+}
